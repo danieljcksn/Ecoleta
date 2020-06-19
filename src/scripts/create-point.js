54957b640b0d04279dc9c3850f292b17fcb6329e
Daniel Cavalcante <45501499+daniielcavallcante@users.noreply.github.com>
2020-06-19 19:45:57 -0300

    Dia 2 - Página Create Point e População de dados nas opções de Input ♻️

---
+++ src/scripts/create-point.js
@@ -0,0 +1,42 @@
+function populateUFs(){
+    const ufSelect = document.querySelector("select[name=uf]")
+
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then((res) => {return res.json()})
+    .then(states => {
+        for(const state of states){
+             ufSelect.innerHTML = ufSelect.innerHTML + `<option value ="${state.id}">${state.nome}</option>`
+        }
+    })
+}
+
+populateUFs()
+
+
+function getCities(event){
+    const citySelect = document.querySelector("select[name=city]")
+    const stateInput = document.querySelector("input[name=state]")
+
+    const ufValue = event.target.value
+
+    const indexOfSelectedState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectedState].text
+
+    //Link atualmente (19/06/2020) não funcionando
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/{${ufValue}}/municipios`
+
+    fetch(url)
+    .then((res) => {return res.json()})
+    .then(cities => {
+        for(const city of cities){
+             citySelect.innerHTML = citySelect.innerHTML + `<option value ="${city.id}">${city.nome}</option>`
+        }
+
+        //Ativa a opção de selecionar a cidade
+        citySelect.disabled = false
+    })
+}
+
+document
+    .querySelector("select[name=uf")
+    .addEventListener("change", getCities)
